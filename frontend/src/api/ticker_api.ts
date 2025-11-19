0960e1a343fb787a9318273a7082dcf9021e9803
Thomas Garcia <thomasakulla@gmail.com>
2025-11-19 03:23:45 -0500

    fixed frontend endpoint, removed useless import

--- frontend/src/api/ticker_api.ts
+++ frontend/src/api/ticker_api.ts
@@ -1,11 +1,10 @@
-import { API_URL } from "@/assets/config";
 import type { Ticker } from "../../../api/src/db/schema";
 
 const TICKER_API_ROUTE = '/api/tickers';
 
 
 export async function searchTicker(tickerSymbol: string): Promise<Ticker | null>  { //convert uppercase always 
-    const response = await fetch(`${API_URL}${TICKER_API_ROUTE}/${tickerSymbol}`, {
+    const response = await fetch(`${TICKER_API_ROUTE}/${tickerSymbol}`, {
         credentials: "include",
     });
 
